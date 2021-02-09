c330cc7fd8e1412d1164d65b5d20f05744ad8419
Frank Jung <frankhjung@linux.com>
2021-02-09 15:57:23 +1100

    update tests

--- test/MultiplySpec.hs
+++ test/MultiplySpec.hs
@@ -12,13 +12,15 @@ import           Test.QuickCheck       (Positive (..))
 spec :: Spec
 spec =
   describe"multiplication algorithms for positive integers" $ do
+    -- valid for positive integers
     prop "multiply0" $
       \(Positive (a :: Word), Positive (b :: Word)) -> multiply0 a b == a * b
     prop "multiply1" $
       \(Positive (a :: Word), Positive (b :: Word)) -> multiply1 a b == a * b
     prop "multiply2" $
       \(Positive (a :: Word), Positive (b :: Word)) -> multiply2 a b == a * b
-    prop "multiply3" $
-      \(Positive (a :: Word), Positive (b :: Word)) -> multiply3 a b == a * b
-    prop "multiply4" $
-      \(Positive (a :: Word), Positive (b :: Word)) -> multiply4 a b == a * b
+    -- valid for non-negative integers
+    prop "multiply3 word" $
+      \(a :: Word, b :: Word) -> multiply3 a b == a * b
+    prop "multiply4 word" $
+      \(a :: Word, b :: Word) -> multiply4 a b == a * b
